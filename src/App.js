0e6e00da00d3a5853e77f62ca6b7333189deb36d
sri-dharshini-revathi <67547021+sri-dharshini-revathi@users.noreply.github.com>
2021-07-30 20:00:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SearchFilter from './components/SearchFilter';
+
+function App() {
+  return (
+    <div className='App'>
+        <SearchFilter />
+    </div>
+  )
+}
+
+export default App;
